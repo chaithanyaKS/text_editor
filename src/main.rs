5831298d5cba9c25d570c24545bc4d8a8a608b3a
chaithanya <chaithanya2934@gmail.com>
2023-10-27 19:08:24 +0530

    completed text viewer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use editor::Editor;
+mod document;
 mod editor;
+mod row;
 mod terminal;
+pub use document::Document;
 pub use editor::Position;
+pub use row::Row;
 pub use terminal::Terminal;
+
 fn main() {
     Editor::default().run();
 }
